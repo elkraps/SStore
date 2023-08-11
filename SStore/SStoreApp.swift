d6d7cbbca87ed80262895a46d2fc7104a6a0cd6a
elkraps <dobroisreal@mail.ru>
2023-08-11 13:59:23 +0300

    Initial Commit

---
+++ SStore/SStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SStoreApp.swift
+//  SStore
+//
+//  Created by Vlad Dzirko on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
